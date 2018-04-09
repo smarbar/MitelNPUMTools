b5ec316acf9884d7734850b918afd78230d2dec6
Scott Barrett <30940685+smarbar@users.noreply.github.com>
2018-04-09 23:16:19 +0100

    Update notes

--- MitelNPUMTools.psm1
+++ MitelNPUMTools.psm1
@@ -111,7 +111,6 @@ Set-BulkNPUMMailboxGreeting 192.168.1.10
 .NOTES
 A folder called Greetings needs to be created on the C Drive of your computer and the properly formated WAV files placed in here for upload. The upload
 procedure will also delete these files so make sure they are a copy and orginial placed somewhere for safe keeping.
-You will be prompted for the mailbox number for each file as it is being uploaded.
 SSH must be enabled on the MSL server.
 
 .Link
@@ -151,6 +150,7 @@ Function Set-BulkNPUMMailboxGreeting
     {
         Write-Output "Filenames cannot include spaces, please change and try again"
         Write-Output "Dont forget to update the csv file with the new filename"
+        Write-Output "You can run Test-BulkNPUM to highlight all those affected"
         return
     }
 
@@ -169,7 +169,7 @@ Function Set-BulkNPUMMailboxGreeting
 
     }
     
-    $csvImport = Import-Csv $FilePath + \mbs.csv
+    $csvImport = Import-Csv $FilePath\mbs.csv
     foreach ($item in $csvImport)
     {
         $File = $item.File
@@ -182,4 +182,49 @@ Function Set-BulkNPUMMailboxGreeting
     Invoke-SSHCommand -SessionId 0 -Command "rmdir /tmp/vf"
     Remove-SFTPSession -Index 0
     Remove-SshSession -Index 0
+}
+
+<#
+.SYNOPSIS
+Tests the audio files specified in the CSV exists and are available for upload
+
+.DESCRIPTION
+The Test-BulkNPUM cmdlet will test that the audio files specified in the CSV exists and are available for upload.
+
+Greetings must be in the below format. 
+CCITT u-law, 8KHz, 8 bit, mono.
+
+A csv file named mbs.csv needs to be stored in the C:\Greetings folder along with the greetings. The csv needs to have two columns, one named 'Mailbox' and the other 'File'
+Mailbox is the mailbox number for the user and file is the file name of the new greeting. Please ensure there are no spaces in the audio filename, this will be highlighted in the test results.
+e.g.
+Mailbox | File
+--------|----------
+1460    | 1460_John.wav
+1678    | Jim1678.wav
+
+.EXAMPLE
+Test-BulkNPUMCSVFile
+
+.NOTES
+A folder called Greetings needs to be created on the C Drive of your computer and the properly formated WAV files placed in here for upload. A CSV file named mbs.csv also needs to be placed here 
+
+
+#>
+Function Test-BulkNPUMUpload
+{
+    $FilePath = "C:\Greetings"
+    $csvImport = Import-Csv $FilePath\mbs.csv
+    foreach ($item in $csvImport)
+    {
+        $File = $item.File
+        $TestPath = Join-Path -Path $FilePath -ChildPath $File
+        If (!(Test-Path -Path $TestPath))
+        {
+            Write-Output "Error:   $File does not exist"
+        }
+        ELSEIF ($file -match ' ')
+        {
+            Write-Output "Warning: $File has spaces in the name"
+        }
+    }
 }
